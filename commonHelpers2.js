f19d07ffb15d893858065b2797b3c1c7beec0df4
Djukipuki <57297352+Djukipuki@users.noreply.github.com>
2023-12-28 15:00:04 +0000

    Deploying to gh-pages from @ Djukipuki/goit-advancedjs-hw-01@778854e1bf521837ff2de35d7efd924ee17b456b 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as m,l as a}from"./assets/vendor-78be7656.js";const i=document.querySelector("iframe"),t=new m(i),r="videoplayer-current-time",e=localStorage.getItem(r);e&&t.setCurrentTime(Number(e));const n=({seconds:o})=>{localStorage.setItem(r,o)};t.on("timeupdate",a(n,1e3));
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as m,l as a}from"./assets/vendor-78be7656.js";const t="videoplayer-current-time",i=document.querySelector("iframe"),r=new m(i),e=localStorage.getItem(t);e&&r.setCurrentTime(Number(e));const n=({seconds:o})=>{localStorage.setItem(t,o)};r.on("timeupdate",a(n,1e3));
 //# sourceMappingURL=commonHelpers2.js.map
