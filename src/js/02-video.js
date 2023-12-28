cd21fd85e79afa65a2096b4c57c3d5cfe2422b97
djukipuki <Angel309933gmail.com>
2023-12-28 17:01:49 +0200

    rename const

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,19 @@
 import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 
-const VIDEO_PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const VIDEO_PLAYER_CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const currentTime = localStorage.getItem(VIDEO_PLAYER_CURRENT_TIME);
+const currentTime = localStorage.getItem(VIDEO_PLAYER_CURRENT_TIME_KEY);
 
 if (currentTime) {
   player.setCurrentTime(Number(currentTime));
 }
 
 const onTimeupdate = ({ seconds }) => {
-  localStorage.setItem(VIDEO_PLAYER_CURRENT_TIME, seconds);
+  localStorage.setItem(VIDEO_PLAYER_CURRENT_TIME_KEY, seconds);
 };
 
 player.on('timeupdate', throttle(onTimeupdate, 1000));
